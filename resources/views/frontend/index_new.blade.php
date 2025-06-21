b29b61bf1ed55c67cec46cef8b2a0ae08e01e469
cvinfotech <chineduanaba@yahoo.com>
2025-06-21 20:31:40 +0530

    twenty one

--- resources/views/frontend/index_new.blade.php
+++ resources/views/frontend/index_new.blade.php
@@ -9,32 +9,17 @@
 @section('content')
 <link href="https://fonts.cdnfonts.com/css/heading" rel="stylesheet">
 <style>
-
-    @import url('https://fonts.googleapis.com/css2?family=Anton&family=Inter:wght@100..900&family=Lunasima:wght@400;700&display=swap');
     .banner-img{
-        border-radius: 22px;
+        border-radius: 16px;
     }
-    .anton-regular {
-        font-family: "Anton", sans-serif !important;
-        font-weight: 400 !important;
-        font-style: normal !important;
-    }
-    .inter-class{
-        font-family: "Inter", sans-serif;
-        font-optical-sizing: auto;
-        font-weight: <weight>;
-        font-style: normal;
-    }
-
     .brows-job-company-img{
     }
     .company_name_sd{
         /* padding: 10px 0px 38px 100px; */
-        padding: 0px 0px 38px 135px;
-        text-align: left;
+        padding: 0px 0px 38px 0px;
+        text-align: center;
         font-weight: bold;
         font-size: 18px;
-        margin-top: -10px;
     }
     .location_sd{
         display: flex;
@@ -82,131 +67,6 @@
         font-size: 30px;
         color: #00e1d8;
     }
-
-    .job-details {
-        display: flex;
-        flex-direction: column;
-        gap: 1rem;
-        text-align: left;
-        padding: 12px 18px 15px;
-        font-family: 
-    }
-
-    .job-info-row {
-        display: flex;
-        align-items: flex-start;
-        gap: 1rem;
-    }
-
-    .job-info-label {
-        font-weight: 400;
-        color: #000;
-        font-size: 18px;
-        white-space: nowrap;
-        width: 145px;
-    }
-
-    .job-info-value {
-        color: #666;
-        line-height: 1.4;
-        font-size: 16px;
-    }
-    
-
-    .view-all-button-main{
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-    }
-    .view-all-button {
-        background-color: #3DE5DA;
-        color: white;
-        font-weight: bold;
-        padding: 16px 32px;
-        border: none;
-        border-radius: 50px;
-        text-align: center;
-        text-decoration: none;
-        font-size: 16px;
-        box-shadow: 0 8px 20px rgba(61, 229, 218, 0.3);
-        transition: background-color 0.3s ease, transform 0.2s ease;
-        text-decoration: none;
-    }
-
-    .view-all-button:hover {
-        /* background-color: #2fd7cd; */
-        transform: translateY(-2px);
-        /* color: white; */
-    background: var(--Secondary, rgba(1, 39, 73, 1));
-    color: white;
-    }
-
-    .recent-added{
-        color: transparent !important;
-        background: linear-gradient(90deg, #31DECD 0%, #6C37EE 100%);
-        background-clip: text;
-    }
-    .vacancies{
-        color: transparent !important;
-        background: var(--Secondary, rgba(1, 39, 73, 1));
-        background-clip: text;
-    }
-.main-block {
-    position: relative;
-    overflow: hidden;
-}
-
-.main-block:hover {
-    box-shadow: 0px 0px 10px #adaaaa;
-}
-
-/* Hide ellipsis on hover */
-.main-block:hover .ellipsis {
-    opacity: 0;
-    pointer-events: none;
-    transition: opacity 0.3s ease;
-}
-
-/* Base button styling */
-.custom-button {
-    position: absolute;
-    bottom: 16px;
-    left: 50%;
-    transform: translate(-50%, 20px); /* start hidden */
-    opacity: 0;
-    transition: transform 0.4s ease, opacity 0.4s ease;
-    width: 93%;
-    background-color: #3DE5DA;
-
-    
-    color: white;
-    font-weight: bold;
-    padding: 15px 40px;
-    border: none;
-    border-radius: 50px;
-    text-align: center;
-    text-decoration: none;
-    font-size: 22px;
-    box-shadow: 0 8px 20px rgba(61, 229, 218, 0.3);
-    margin: 0 auto;
-    
-    /* other styles as you had */
-}
-/* Hover effect to slide and show the button */
-.main-block:hover .custom-button {
-    transform: translate(-50%, 0); /* slide up into view */
-    opacity: 1;
-}
-
-/* Button hover effect */
-.custom-button:hover {
-    /* background-color: #2fd7cd; */
-    background: var(--Secondary, rgba(1, 39, 73, 1));
-    color: white;
-
-    transform: translateY(-2px);
-}
-
 </style>
     <div class="clearfix"></div>
     <section class="banner trans banner-custom lazyload" data-src="{{url('assets/frontend')}}/img/slider-1.jpg" data-overlay="6">
@@ -261,19 +121,15 @@
         <div class="container">
             <div class="row">
                 <div class="main-heading">
-                    <h2 style="text-align: left; font-size: 50px;" class="anton-regular">
-                        <span class="recent-added">Recent Added</span> <span class="vacancies">Vacancies</span>
+                    <h2 style="text-align: left">
+                        <span style="
+color: transparent;
+background: linear-gradient(90deg, #31DECD 0%, #6C37EE 100%);
+background-clip: text;">Recent Added</span> <span style="color: transparent;
+background: var(--Secondary, rgba(1, 39, 73, 1));
+background-clip: text;">Vacancies</span>
                     </h2>
-                    <div class="view-all-button-main">
-                        <p style="text-align: left; font-size: 20px; width: 50%;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna.</p>
-                        <div>
-                            <a href="{!! route('home.jobList') !!}" class="view-all-button">View All
-                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M17.293 7.29298C17.6591 6.92686 18.2381 6.90427 18.6309 7.22462L18.707 7.29298L22.707 11.293L22.7754 11.3692C23.0957 11.7619 23.0731 12.3409 22.707 12.707L18.707 16.707C18.3165 17.0976 17.6835 17.0976 17.293 16.707C16.9024 16.3165 16.9024 15.6835 17.293 15.293L19.5859 13H2C1.44772 13 1 12.5523 1 12C1 11.4477 1.44772 11 2 11H19.5859L17.293 8.70704L17.2246 8.63087C16.9043 8.2381 16.9269 7.65909 17.293 7.29298Z" fill="white"/>
-                                </svg>
-                            </a>
-                        </div>
-                    </div>
+                    <p style="text-align: left; font-size: 20px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna.</p>
                 </div>
             </div>
             <div class="row extra-mrg">
@@ -301,17 +157,18 @@
                                 $company_cover_img = url('uploads').'/client_profile/'.$client_data->cover_image;
                             }
                         @endphp
-                        <div class="col-xl-4 col-md-6 col-12">
+                        <div class="col-md-4 col-sm-6">
                             <a href="{{route('getJobDetail',['id' => $value->slug])}}">
-                                <div class="grid-view brows-job-list p-0  main-block" style="border-radius:35px;">
-                                    <div class="banner-img lazyload" data-src="{{$company_cover_img}}" style="margin: 15px 15px 0px 15px;">
-                                        <div class="brows-job-company-img company-img-logo mt-0 mb-0">
-                                            <img data-src="{{$company_logo}}" class="img-responsive lazyload" alt="">
-                                        </div>                                        
+                                <div class="grid-view brows-job-list p-0">
+                                    <div class="banner-img lazyload" data-src="{{$company_cover_img}}">
+                                        
+                                    </div>
+                                    <div class="brows-job-company-img company-img-logo mt-0 mb-0">
+                                        <img data-src="{{$company_logo}}" class="img-responsive lazyload" alt="">
                                     </div>
                                     
                                     <div class="company_name_sd">
-                                        <p class="p-0 m-0"><span class="inter-class">{!! $company_name !!}</span></p>
+                                        <p class="p-0 m-0"><span>{!! $company_name !!}</span></p>
                                     </div>
 
                                     <div class="job-position">
@@ -321,29 +178,16 @@
                                         <h3>@if(isset($value->jobtitle) && !empty($value->jobtitle)){!! $value->jobtitle !!}@endif</h3>
                                     </div>
 
-
-                                    <div class="job-details">
-                                        <div class="job-info-row">
-                                            <div class="job-info-label anton-regular">Location</div>
-                                            <div class="job-info-value inter-class">
-                                                @if(isset($value->altlocation) && !empty($value->altlocation))
-                                                    {!! $value->altlocation !!}
-                                                @endif
-                                            </div>
-                                        </div>
-
-                                        <div class="job-info-row">
-                                            <div class="job-info-label anton-regular">Offered Salary</div>
-                                            <div class="job-info-value inter-class">
-                                                UP TO £
-                                                @if(isset($value->rateupper) && !empty($value->rateupper))
-                                                    {!! App\CustomFunction\CustomFunction::number_format($value->rateupper) !!}
-                                                @endif
-                                                DOE
-                                            </div>
-                                        </div>
+                                    <div class="location_sd">
+                                        <div class="location_first_sd">Location</div>
+                                        <div class="location_second_sd">@if(isset($value->altlocation) && !empty($value->altlocation)){!! $value->altlocation !!}@endif</div>
                                     </div>
 
+                                    
+                                    <div class="salary_sd">
+                                        <div class="salary_first_sd">Offered Salary</div>
+                                        <div class="salary_second_sd">UP TO £@if(isset($value->rateupper) && !empty($value->rateupper)){!! App\CustomFunction\CustomFunction::number_format($value->rateupper) !!}@endif DOE</div>
+                                    </div>
 
 
 
@@ -359,7 +203,7 @@
                                                 <span class="full-time  custom-full-time">Fixed term contract</span>
                                             @endif
                                             @if($value->jobtenure == 'temporary')
-                                                <span class="freelanc custom-freelanc" style="background: rgba(253, 251, 202, 1);">Temporary</span>
+                                                <span class="freelanc custom-freelanc">Temporary</span>
                                             @endif
                                         @endif
                                     </div>
@@ -370,14 +214,6 @@
                                         <div>.</div>
                                     </div>
 
-                                    <a href="{{route('getJobDetail',['id' => $value->slug])}}" class="custom-button">Find Out More</a>
-
-                                    {{-- <div class="custom-button-wrapper">
-    <a href="{{route('getJobDetail',['id' => $value->slug])}}" class="custom-button">Find Out More</a>
-</div> --}}
-
-
-
                                     
                                 </div>
                             </a>
@@ -387,6 +223,9 @@
             </div>
             <div class="row">
                 <div class="col-md-12 mt-5 text-center">
+                    <div class="ur-detail-btn">
+                        <a href="{!! route('home.jobList') !!}" class="btn btn-primary mrg-bot-15">View More</a>
+                    </div>
                 </div>
             </div>
         </div>
